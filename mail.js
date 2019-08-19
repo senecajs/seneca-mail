8ee2504090340df19c1a3e219fc2a0de9581ea9a
rjrodger <richard@ricebridge.com>
2019-08-19 16:26:24 -0700

    v1.0.0

--- mail.js
+++ mail.js
@@ -1,11 +1,9 @@
 /* Copyright (c) 2013-2019 Richard Rodger and other contributors, MIT License */
-"use strict"
+"use strict";
 
+const Email = require("email-templates");
 
-const Email = require('email-templates')
-
-
-module.exports = mail
+module.exports = mail;
 module.exports.defaults = {
   test: false,
   email: {
@@ -13,78 +11,78 @@ module.exports.defaults = {
     send: false,
     preview: false
   }
-}
-
-
+};
 
 function mail(options) {
-  var seneca = this
-  var mailer
-  
-  // TODO seneca 4.x with embedded promisify should support this
-  seneca.depends('promisify')
-  
-  
-  seneca
-    .message('sys:mail,send:mail', send_mail)
-    .message('sys:mail,hook:render', hook_render)
+  var seneca = this;
+  var mailer;
 
-  
+  // TODO seneca 4.x with embedded promisify should support this
+  seneca.depends("promisify");
+
+  seneca
+    .message("sys:mail,send:mail", send_mail)
+    .message("sys:mail,hook:render", hook_render);
+
   seneca.prepare(async function() {
-    var root = seneca.root
-    
-    var email_opts = options.email 
+    var root = seneca.root;
+
+    var email_opts = options.email;
 
     // create transport
-    var transport_opts = email_opts.transport
-    if(transport_opts) {
-      var transport_maker = require('nodemailer-'+transport_opts.name)
-      var transport = transport_maker(transport_opts.options)
-      email_opts.transport = transport
+    var transport_opts = email_opts.transport;
+    if (transport_opts) {
+      var transport_maker = require("nodemailer-" + transport_opts.name);
+      var transport = transport_maker(transport_opts.options);
+      email_opts.transport = transport;
     }
 
     email_opts.render = async (view, content) => {
-      var code = view.split('/')[0]
-      var part = view.split('/')[1]
-      
+      var code = view.split("/")[0];
+      var part = view.split("/")[1];
+
       // TODO: how to make this action specific?
-      var res = await root.post('sys:mail,hook:render', {code,part,content})
-      var html = await mailer.juiceResources(res.html)
+      var res = await root.post("sys:mail,hook:render", {
+        code,
+        part,
+        content
+      });
+      var html = await mailer.juiceResources(res.html);
 
-      return html
-    }
-    
-    mailer = new Email(email_opts) 
-  })
+      return html;
+    };
 
+    mailer = new Email(email_opts);
+  });
 
   async function send_mail(msg) {
-    var content = msg.content
+    var content = msg.content;
 
     var mail_opts = {
       template: msg.code,
       message: {
         to: msg.to,
         from: msg.from,
-        subject: msg.subject,
+        subject: msg.subject
       },
       locals: content
-    }
+    };
 
-    var res = await mailer.send(mail_opts)
+    var res = await mailer.send(mail_opts);
 
     return {
       msg: msg,
       sent: res
-    }
+    };
   }
 
-  
   async function hook_render(msg) {
     return {
-      html: 'NO RENDER DEFINED FOR '+msg.code+
-        ', content was: '+JSON.stringify(msg.content)
-    }
+      html:
+        "NO RENDER DEFINED FOR " +
+        msg.code +
+        ", content was: " +
+        JSON.stringify(msg.content)
+    };
   }
-}  
-  
+}
